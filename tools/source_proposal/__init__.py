40728cc199b612c04acf07967ee48864114902da
Sergey303 <s-agittariu-s@mail.ru>
2026-08-04 15:40:43 +0300

    Export source proposal activation

--- tools/source_proposal/__init__.py
+++ tools/source_proposal/__init__.py
@@ -2,9 +2,17 @@ from .common import SourcePipelineError, load_schemas
 from .acquire import snapshot_source, fragment_workspace, prepare_extraction
 from .proposal import import_assertion_proposal, import_grounding_review
 from .gate import execute_gate, verify_package
+from .activation import stage_activation
 
 __all__ = [
-    "SourcePipelineError", "load_schemas", "snapshot_source", "fragment_workspace",
-    "prepare_extraction", "import_assertion_proposal", "import_grounding_review",
-    "execute_gate", "verify_package",
+    "SourcePipelineError",
+    "load_schemas",
+    "snapshot_source",
+    "fragment_workspace",
+    "prepare_extraction",
+    "import_assertion_proposal",
+    "import_grounding_review",
+    "execute_gate",
+    "verify_package",
+    "stage_activation",
 ]
