612884471a662de856b95b33305e80b4f2455a83
Sergey303 <s-agittariu-s@mail.ru>
2026-08-04 11:52:56 +0300

    Export portable PDF reader adapters

--- tools/source_proposal/pdf_link.py
+++ tools/source_proposal/pdf_link.py
@@ -1,12 +1,32 @@
 from .pdf_common import (
-    MAX_PDF_BYTES, PDF_IR_DOMAIN, PDF_RECORD_DOMAIN, PDF_SEED_DOMAIN, load_pdf_schemas,
+    MAX_PDF_BYTES,
+    PDF_IR_DOMAIN,
+    PDF_RECORD_DOMAIN,
+    PDF_SEED_DOMAIN,
+    load_pdf_schemas,
 )
-from .pdf_parser import fetch_pdf, parse_pdf_with_poppler
+from .pdf_parser import (
+    SUPPORTED_PDF_READERS,
+    fetch_pdf,
+    parse_pdf,
+    parse_pdf_with_poppler,
+    parse_pdf_with_pypdf,
+)
 from .pdf_seed import resolve_pdf_seed
 from .pdf_workspace import fragments_from_ir, ingest_pdf_link
 
 __all__ = [
-    "MAX_PDF_BYTES", "PDF_IR_DOMAIN", "PDF_RECORD_DOMAIN", "PDF_SEED_DOMAIN",
-    "load_pdf_schemas", "fetch_pdf", "parse_pdf_with_poppler",
-    "fragments_from_ir", "ingest_pdf_link", "resolve_pdf_seed",
+    "MAX_PDF_BYTES",
+    "PDF_IR_DOMAIN",
+    "PDF_RECORD_DOMAIN",
+    "PDF_SEED_DOMAIN",
+    "SUPPORTED_PDF_READERS",
+    "load_pdf_schemas",
+    "fetch_pdf",
+    "parse_pdf",
+    "parse_pdf_with_poppler",
+    "parse_pdf_with_pypdf",
+    "fragments_from_ir",
+    "ingest_pdf_link",
+    "resolve_pdf_seed",
 ]
