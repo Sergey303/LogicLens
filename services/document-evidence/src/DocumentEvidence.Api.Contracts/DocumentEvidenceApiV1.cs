698b9b1d9bda1d9e99eb08e15a227fea66150751
Sergey303 <s-agittariu-s@mail.ru>
2026-08-05 11:03:31 +0300

    Pin versioned document evidence routes

---
+++ services/document-evidence/src/DocumentEvidence.Api.Contracts/DocumentEvidenceApiV1.cs
@@ -0,0 +1,25 @@
+namespace KnowledgePilot.LogicLens.DocumentEvidence.Api.Contracts;
+
+public static class DocumentEvidenceApiV1
+{
+    public const string ActorHeader = "X-Actor-Id";
+    public const string FileNameHeader = "X-File-Name";
+    public const string IdempotencyHeader = "Idempotency-Key";
+    public const string SourceKindHeader = "X-Source-Kind";
+    public const string Prefix = "/api/v1";
+
+    public static string UploadRevision(Guid workspaceId, Guid documentId) =>
+        $"{Prefix}/workspaces/{workspaceId:D}/documents/{documentId:D}/revisions";
+
+    public static string Document(Guid workspaceId, Guid documentId) =>
+        $"{Prefix}/workspaces/{workspaceId:D}/documents/{documentId:D}";
+
+    public static string Fragments(Guid workspaceId, Guid revisionId) =>
+        $"{Prefix}/workspaces/{workspaceId:D}/revisions/{revisionId:D}/fragments";
+
+    public static string Processing(Guid workspaceId, Guid jobId) =>
+        $"{Prefix}/workspaces/{workspaceId:D}/processing-jobs/{jobId:D}";
+
+    public static string ReadPlan(Guid workspaceId, Guid revisionId) =>
+        $"{Prefix}/workspaces/{workspaceId:D}/revisions/{revisionId:D}/read-plans";
+}
